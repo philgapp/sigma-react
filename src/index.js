b3d6755d458cab2450c4311679c87f7dc44e7a55
Phillip <philgapp@gmail.com>
2021-04-01 09:03:52 +0200

    Apollo Client
    - installed Apollo with yarn add @apollo/client graphql
    - replaced index.js content with tutorial content to wire in Apollo

--- src/index.js
+++ src/index.js
@@ -2,16 +2,32 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/index.css';
 import App from './components/App';
-import reportWebVitals from './reportWebVitals';
+import * as serviceWorker from './serviceWorker';
 
+// 1
+import {
+    ApolloProvider,
+    ApolloClient,
+    createHttpLink,
+    InMemoryCache
+} from '@apollo/client';
+
+// 2
+const httpLink = createHttpLink({
+    uri: 'http://localhost:4000'
+});
+
+// 3
+const client = new ApolloClient({
+    link: httpLink,
+    cache: new InMemoryCache()
+});
+
+// 4
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <ApolloProvider client={client}>
+        <App />
+    </ApolloProvider>,
+    document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+serviceWorker.unregister();
